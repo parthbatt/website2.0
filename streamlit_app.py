1516728d7058e17903fc594366c4157399121e61
parthbatt <143800471+parthbatt@users.noreply.github.com>
2023-09-06 17:44:58 +0530

    streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="parth''s website", page_icon=":tada:", layout="wide")
+st.subheader("hi i am parth")
+st.write("---")
+st.header("what do i do")
+st.write("i'am an python devloper and i  will make a python tutorial to make this website and deploy it to streamlit.com/cloud ")
+st.header('why i made this') 
+st.write("for fun")
